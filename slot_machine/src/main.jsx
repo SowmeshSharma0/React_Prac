a84668a6fef9e7d9604deb8202b4092db58c6a41
Sowmesh Sharma H M <Sowmesh.sharma-hm@sixt.com>
2025-01-17 11:55:56 +0530

    basic props exercise

---
+++ slot_machine/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App num1={5} num2={7} num3={5}/>
+  </StrictMode>,
+)
