f220a99c7b0751b5e11d3646bee2aef04b0de77b
Sowmesh Sharma H M <Sowmesh.sharma-hm@sixt.com>
2025-01-20 13:54:50 +0530

    multi counter with complex object mapping and setStates

--- score_keeper_exercise/src/li_ele.jsx
+++ score_keeper_exercise/src/li_ele.jsx
@@ -1,17 +1,27 @@
-function Li_Ele({ele_id, playerScore, target, scores, setScores}){
-    function updateScore(index){
-        const new_scores = scores.map((c, ind) =>{
-            if(index == ind && c < target)
-                return c+1;
+function Li_Ele({score_id, playerScore, target, scores, setScores, hasWinner}){
+    // console.log(score_id)
+    // for (let score of scores){
+    //     console.log(score)
+    // }
+    function updateScore(score_id){
+        const new_scores = scores.map((score_ele) =>{
+            if(score_id == score_ele.id && score_ele.score < target)
+                return {
+                    ...score_ele,
+                    score: score_ele.score+1
+                }
             else
-                return c;
+                return score_ele;
         })
         setScores(new_scores)
     }
     return (
-        <li key={ele_id}>
-            <h2 style={{display: "inline-block"}}>Player{ele_id + 1} : {playerScore} </h2>
-            <button onClick={()=>updateScore(ele_id) } className="increment">+1</button>
+        <li>
+            <h2 style={{display: "inline-block"}}>Player{score_id} : {playerScore} </h2>
+            <button 
+                onClick={()=>updateScore(score_id)}
+                disabled={hasWinner}
+            >+1</button>
             {playerScore === target ? <h3 style={{display: "inline-block"}}> Winner!!!!</h3> : null}
         </li>
     )
--- score_keeper_exercise/src/scorekeeper.jsx
+++ score_keeper_exercise/src/scorekeeper.jsx
@@ -1,22 +1,43 @@
 import { useState } from "react";
-// import { v4 as uuid } from "uuid";
+import { v4 as uuid } from "uuid";
 import Li_Ele from "./li_ele";
 
 function ScoreKeeper({numPlayers, target}){
 
-    const setInitScores = ()=> new Array(numPlayers).fill(0)
+    //arr of objects with each object containing id, score
+    const setInitScores = ()=> Array.from({length:numPlayers}, () => (
+        {
+            id: uuid(),
+            score: 0
+        }
+    ))
+    
     const [scores, setScores] = useState(setInitScores)
+    const hasWinner = scores.some((ele) => ele.score>=target)
 
     function ResetScores(){
-        const reinit_scores = new Array(numPlayers).fill(0)
+        const reinit_scores = Array.from({length:numPlayers}, () => (
+            {
+                id: uuid(),
+                score: 0
+            }
+        ))
         setScores(reinit_scores)
     }
 
     return (
         <>
             <ul>
-                {scores.map((score,index) => (
-                    <Li_Ele key={index} ele_id={index} playerScore={score} target={target} scores={scores} setScores={setScores}/>
+                {scores.map((score_ele) => (
+                    <Li_Ele 
+                        key={score_ele.id} 
+                        score_id={score_ele.id} 
+                        playerScore={score_ele.score} 
+                        target={target} 
+                        scores={scores} 
+                        setScores={setScores}
+                        hasWinner={hasWinner}
+                    />
                 ))}
             </ul>
             <button onClick={ResetScores}>Reset</button>
