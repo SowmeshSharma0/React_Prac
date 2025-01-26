d69949fcb8a4c2e300c67064f438421e64d71da0
Sowmesh Sharma H M <Sowmesh.sharma-hm@sixt.com>
2025-01-26 17:44:49 +0530

    added swap btn logic + wrapped fectching optns in custom hook

--- currency_exchanger/src/exchanger.jsx
+++ currency_exchanger/src/exchanger.jsx
@@ -1,30 +1,30 @@
 import {Button, CurrencyContainer} from "./components/index.js"
 import './exchanger.css';
-import getData from "./services/getCurrencyinfo.js";
+import useOptions from "./hooks/useOptions.jsx";
 
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
 function Exchanger() {
   const [FromVal, setFromVal] = useState(0)
   const [ToVal, setToVal] = useState(0)
   const [FromCurrency, setFromCurrency] = useState("usd")
   const [ToCurrency, setToCurrency] = useState("inr")
-  const [Options, setOptions] = useState({})
-
-  // getData("usd").then(data => console.log(data))
-  useEffect(() => {
-    getData(FromCurrency).then(data => setOptions(data[FromCurrency]))   
-  }, [FromCurrency]);
+  const Options = useOptions(FromCurrency)
 
   const handleClick = () => {
     console.log(Options)
     setToVal(FromVal * Options[ToCurrency])
   }
 
+  const swapCurrencyTypes = () => {
+    setFromCurrency(ToCurrency)
+    setToCurrency(FromCurrency)
+  }
+
   return (
     <div className="Exchanger">
         <CurrencyContainer purposeStr={"from"} value={FromVal} selectedOption={FromCurrency} options={Object.keys(Options)} isDisabled={false} setVal={setFromVal} setCurrency={setFromCurrency}/>
-        <Button str={"Swap"}/>
+        <Button str={"Swap"} onClick={swapCurrencyTypes}/>
         <CurrencyContainer purposeStr={"to"} value={ToVal} selectedOption={ToCurrency} options={Object.keys(Options)} isDisabled={true} setVal={setToVal} setCurrency={setToCurrency}/>
         <Button str={"Convert INR to USD"} onClick={handleClick}/>
     </div>
