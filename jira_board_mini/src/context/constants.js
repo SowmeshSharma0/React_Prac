406a7c2aae42156f4a27796ebd651be6f118cae7
Sowmesh Sharma H M <Sowmesh.sharma-hm@sixt.com>
2025-02-18 12:56:06 +0530

    added drop back rule as well

--- jira_board_mini/src/context/constants.js
+++ jira_board_mini/src/context/constants.js
@@ -23,11 +23,12 @@ export const can_delete_at_cross_axis_state = {
     3: true
 }
 
+//can change drag rules here; convenient way to change the rules; no merge conflicts
 export const card_move_rules_horizontal = {
     0: [1],
-    1: [2],
-    2: [3],
-    3: []
+    1: [0, 2],
+    2: [1, 3],
+    3: [2]
 }
 
 export const card_move_rules_vertical_cross_state = {
