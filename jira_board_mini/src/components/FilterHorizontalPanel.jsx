bbfeeb1ca4d2bdf9a6127f97333e563efd543da7
Sowmesh Sharma H M <Sowmesh.sharma-hm@sixt.com>
2025-02-18 13:36:54 +0530

    added filter heading + some styling change to filter btns

--- jira_board_mini/src/components/FilterHorizontalPanel.jsx
+++ jira_board_mini/src/components/FilterHorizontalPanel.jsx
@@ -1,44 +1,34 @@
 import { useContext } from "react"
 import { CardContext } from "../context/CardContext"
-import { StyledFilterBox, StyledFilterHorizontalPanel } from "./styles/FilterHorizontalPanel.styled"
-import ToggleOnIcon from '@mui/icons-material/ToggleOn';
-import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import { StyledFilterBox, StyledFilterHorizontalPanel, StyledFilterPanelContainer } from "./styles/FilterHorizontalPanel.styled"
+// import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+// import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 
 const FilterHorizontalPanel = () => {
 
     const {Assignees, toggleAssigneeFilter} = useContext(CardContext)
-    // console.log("Assignees: ", Assignees)
     return (
-        <StyledFilterHorizontalPanel len={Assignees.size}>
-            {Object.entries(Assignees).map(([key, value], idx) => {
-                return (
+        <StyledFilterPanelContainer>
+            <h2>Quick Filters :</h2>
+            <StyledFilterHorizontalPanel len={Assignees.size}>
+                {Object.entries(Assignees).map(([key, value], idx) => {
+                    return (
                     <StyledFilterBox 
                         key={idx} 
                         isToggleActive={value.isFilterActive}
                         onClick={() => {
                             toggleAssigneeFilter(key)
-                            // console.log("Toggled: ", assignee)
                         }}
                     >
-                        <p>{key}</p>
-                        {value.isFilterActive ? <ToggleOnIcon id="toggle-on"/> : <ToggleOffIcon id="toggle-off"/>}
-                        {/* <label htmlFor={assignee.assignee}>{assignee.assignee}</label>
-                        <input 
-                            type="checkbox" 
-                            id={assignee.assignee} 
-                            name={assignee.assignee} 
-                            value={assignee.assignee}
-                            onChange={() => {
-                                toggleAssigneeFilter(assignee)
-                                console.log("Toggled: ", assignee)
-                            }}
-                            defaultValue={assignee.isFilterActive}
-                            checked={assignee.isFilterActive}
-                        /> */}
+                        <div className="filter-box-content">
+                            <p>{key}</p>
+                            {/* {value.isFilterActive ? <ToggleOnIcon id="toggle-on"/> : <ToggleOffIcon id="toggle-off"/>} */}
+                        </div>
                     </StyledFilterBox>
                 )
             })}
-        </StyledFilterHorizontalPanel>
+            </StyledFilterHorizontalPanel>
+        </StyledFilterPanelContainer>
     )
 }
 
--- jira_board_mini/src/components/styles/FilterHorizontalPanel.styled.js
+++ jira_board_mini/src/components/styles/FilterHorizontalPanel.styled.js
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 
+export const StyledFilterPanelContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h2{
+        font-size: 1.5rem;
+        font-weight: 600;
+        margin: 0;
+        padding: 0;
+        margin-bottom: 1rem;
+        color: #4a4a4a;
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+    }
+`
+
 export const StyledFilterHorizontalPanel = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     width: fit-content;
     padding: ${({len}) => len > 0 ? '1.2rem' : '0'};
-    background-color: #f1f1f1;
-    /* background-color: red; */
+    /* background-color: #f1f1f1; */
     margin: 1rem;
     border-radius: 10px;
     box-shadow: 0px 0px 5px 0px rgb(67, 66, 66);
@@ -18,13 +32,24 @@ export const StyledFilterBox = styled.div`
     flex-direction: row;
     align-items: center;
     margin: 0.5rem;
-    padding: 0.5rem;
+    /* padding: 0.5rem; */
     border-radius: 5px;
     box-shadow: 0px 0px 5px 0px #000000;
     transition: all 0.3s ease;
-    p{
-        margin: 0;
-        font-weight: ${({isToggleActive}) => isToggleActive ? '600' : '400'};
+    .filter-box-content{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        p{
+            margin: 0;
+            font-weight: ${({isToggleActive}) => isToggleActive ? '600' : '400'};
+            font-size: ${({isToggleActive}) => isToggleActive ? '1rem' : '1.06rem'};
+            color: white;
+        }
+        padding: 0.5rem;
+        border-radius: 5px;
+        background-color: ${({isToggleActive}) => isToggleActive ? 'green' : 'red'};
     }
     &:hover{
         cursor: pointer;
@@ -34,12 +59,12 @@ export const StyledFilterBox = styled.div`
         transform: scale(1.05);
     }
 
-    #toggle-on{
+    /* #toggle-on{
         color: green;
         margin-left: 0.5rem;
     }
     #toggle-off{
         color: red;
         margin-left: 0.5rem;
-    }
+    } */
 `
