1b4a7295d6d0552901cc947e6b2fb0db68061877
Sowmesh Sharma H M <Sowmesh.sharma-hm@sixt.com>
2025-02-27 12:40:42 +0530

    Commented out dark mode btn; cz it wasnt very nice looking

--- jira_board_mini/src/App.jsx
+++ jira_board_mini/src/App.jsx
@@ -6,35 +6,35 @@ import { GlobalContext } from './context/GlobalContext'
 import FilterHorizontalPanel from './components/FilterHorizontalPanel'
 import HorizontalListView from './components/HorizontalListView'
 import { ThemeContext } from './context/ThemeContext'
-import ToggleOnIcon from '@mui/icons-material/ToggleOn';
-import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+// import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+// import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 
 function App() {
   const {main_axis_IsExpandable_init} = useContext(GlobalContext)
-  const {isDark, setIsDark} = useContext(ThemeContext)
+  // const {isDark, setIsDark} = useContext(ThemeContext)
 
-  const toggleTheme = () => setIsDark(!isDark)
+  // const toggleTheme = () => setIsDark(!isDark)
 
-  useEffect(() => {
-    const app_container = document.querySelector('.App-container')
-    if(isDark){
-      app_container.classList.remove('light')
-      app_container.classList.add('dark')
-    }
-    else{
-      app_container.classList.remove('dark')
-      app_container.classList.add('light')
-    }
-  }, [isDark])
+  // useEffect(() => {
+  //   const app_container = document.querySelector('html')
+  //   if(isDark){
+  //     app_container.classList.remove('light')
+  //     app_container.classList.add('dark')
+  //   }
+  //   else{
+  //     app_container.classList.remove('dark')
+  //     app_container.classList.add('light')
+  //   }
+  // }, [isDark])
 
   return (
       <div className="App-container">
         <div className="header">
           <h1>Ticket Manager Mini</h1>
-          {isDark ? 
+          {/* {isDark ? 
             <ToggleOnIcon onClick={toggleTheme} className='toggle-on'/> 
             : <ToggleOffIcon onClick={toggleTheme} className='toggle-off'/>
-          }
+          } */}
         </div>
         <FilterHorizontalPanel/>
         <div className="horizontal-scroll_wrapper">
